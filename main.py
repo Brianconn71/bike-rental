d7d872411ecae4dbf8bf19fc402e95526349bf66
Brian Connolly <brian.connolly71@gmail.com>
2022-08-28 15:54:42 +0100

    adding testing to class and methods

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ class BikeRental:
         Displays bikes currently available for hire
         """
         print("We have currently {} bikes available for hire".format(self.stock))
+        return self.stock
 
     def rentBikeOnHourlyBasis(self, n):
         """
@@ -26,7 +27,7 @@ class BikeRental:
             return None
         # do not rent a bike if stock is less than requested bikes
         elif n > self.stock:
-            print("Sorry! We have currently {self.stock} bikes available to rent!".format(self.stock))
+            print("Sorry! We have currently {} bikes available to rent!".format(self.stock))
             return None
         # Now we can rent bikes
         else:
@@ -38,91 +39,91 @@ class BikeRental:
             self.stock -= n
             return now
 
-        def rentBikeOnDailyBasis(self, n):
-            """
-            Rentas a bike on a daily basis to a customer
+    def rentBikeOnDailyBasis(self, n):
+        """
+        Rentas a bike on a daily basis to a customer
 
-            Args:
-                n (_type_): _description_
-            """                           
+        Args:
+            n (_type_): _description_
+        """                           
 
-            if n <=0:
-                print("Number of bikes must be greater than 0")
-                return None
-            elif n > self.stock:
-                print("Sorry! We currently have {} bikes awailable to rent".format(self.stock))
-                return None
-            else:
-                now = datetime.datetime.now()
-                print("You have rented {} bike/s on a daily basis today at {}".format(n, now.hour))
-                print("You will be charged $20 for each day per bike")
-                print("We hope that you enjoy our service.")
+        if n <=0:
+            print("Number of bikes must be greater than 0")
+            return None
+        elif n > self.stock:
+            print("Sorry! We currently have {} bikes awailable to rent".format(self.stock))
+            return None
+        else:
+            now = datetime.datetime.now()
+            print("You have rented {} bike/s on a daily basis today at {}".format(n, now.hour))
+            print("You will be charged $20 for each day per bike")
+            print("We hope that you enjoy our service.")
 
-                self.stock -= n
-                return now
+            self.stock -= n
+            return now
+    
+    def rentBikeOnWeeklyBasis(self, n):
+        """
+        Rents a bike on a weekly basis to a customer
+
+        Args:
+            n (_type_): _description_
+        """    
+        if n <=0:
+            print("Number of bikes must be greater than 0")
+            return None
+        elif n > self.stock:
+            print("Sorry! We currently have {} bikes awailable to rent".format(self.stock))
+            return None
+        else:
+            now = datetime.datetime.now()
+            print("You have rented {} bike/s on a weekly basis today at {}".format(n, now.hour))
+            print("You will be charged $60 for each week per bike")
+            print("We hope that you enjoy our service.")
+
+            self.stock -= n
+            return now    
+
+    def returnBike(self, request):
+        """
+        1. Accept a rented bike from a customer
+        2. Replenish the inventory
+        3. Return a bill
+
+        Args:
+            request (_type_): _description_
+        """               
+        # extract the tuple and initiate bill
+        rentalTime, rentalBasis, numOfBikes = request
+        bill = 0
+
+        # issue a bill only if all three params are not null
+        if rentalTime and rentalBasis and numOfBikes:
+            self.stock += numOfBikes
+            now = datetime.datetime.now()
+            rentalPeriod = now - rentalTime
         
-        def rentBikeOnWeeklyBasis(self, n):
-            """
-            Rents a bike on a weekly basis to a customer
+            # hourly bill calculation
+            if rentalBasis == 1:
+                bill = round(rentalPeriod.seconds / 3600) * 5 * numOfBikes
 
-            Args:
-                n (_type_): _description_
-            """    
-            if n <=0:
-                print("Number of bikes must be greater than 0")
-                return None
-            elif n > self.stock:
-                print("Sorry! We currently have {} bikes awailable to rent".format(self.stock))
-                return None
-            else:
-                now = datetime.datetime.now()
-                print("You have rented {} bike/s on a weekly basis today at {}".format(n, now.hour))
-                print("You will be charged $60 for each week per bike")
-                print("We hope that you enjoy our service.")
+            # daily bill calculation
+            elif rentalBasis == 2:
+                bill = round(rentalPeriod.days) * 20 * numOfBikes
+            # Weekly calculation
+            elif rentalBasis == 3:
+                bill = round(rentalPeriod.days) * 20 * numOfBikes
+            # family discount calculation
+            if (3 <= numOfBikes <=5):
+                print("You are eligible for a family discount of 30%")
+                bill = bill * 0.7
 
-                self.stock -= n
-                return now    
-
-        def returnBike(self, request):
-            """
-            1. Accept a rented bike from a customer
-            2. Replenish the inventory
-            3. Return a bill
-
-            Args:
-                request (_type_): _description_
-            """               
-            # extract the tuple and initiate bill
-            rentalTime, rentalBasis, numOfBikes = request
-            bill = 0
-
-            # issue a bill only if all three params are not null
-            if rentalTime and rentalBasis and numOfBikes:
-                self.stock += numOfBikes
-                now = datetime.datetime.now()
-                rentalPeriod = now - rentalTime
-            
-                # hourly bill calculation
-                if rentalBasis == 1:
-                    bill = round(rentalPeriod.seconds / 3600) * 5 * numOfBikes
-
-                # daily bill calculation
-                elif rentalBasis == 2:
-                    bill = round(rentalPeriod.days) * 20 * numOfBikes
-                # Weekly calculation
-                elif rentalBasis == 3:
-                    bill = round(rentalPeriod.days) * 20 * numOfBikes
-                # family discount calculation
-                if (3 <= numOfBikes <=5):
-                    print("You are eligible for a family discount of 30%")
-                    bill = bill * 0.7
-
-                print("Thanks for returning your bike. I hope you enjoyed your ride ;)")
-                print("That will be ${}".format(bill))
-                return bill
-            else:
-                print("Are you sure you rented a bike with us pal?")
-                return None
+            print("Thanks for returning your bike. I hope you enjoyed your ride ;)")
+            print("That will be ${}".format(bill))
+            return bill
+        else:
+            print("Are you sure you rented a bike with us pal?")
+            return None
 
 
 class Customer:
--- tests.py
+++ tests.py
@@ -7,5 +7,66 @@ class BikeRentalTest(unittest.TestCase):
     def test_Bike_Rental_Displays_correct_stock(self):
         shop1 = BikeRental()
         shop2 = BikeRental(10)
-        self.assertEqual(shop1.displaystock(), 0)
-        self.assertEqual(shop2.displaystock(), 10)
+        self.assertEquals(shop1.displaystock(), 0)
+        self.assertEquals(shop2.displaystock(), 10)
+    
+    def test_rentBikeOnHourlyBasis_for_negative_number_of_bikes(self):
+        shop = BikeRental(10)
+        self.assertEqual(shop.rentBikeOnHourlyBasis(-1), None)
+    
+    def test_rentBikeOnHourlyBasis_for_zero_number_of_bikes(self):
+        shop = BikeRental(10)
+        self.assertEqual(shop.rentBikeOnHourlyBasis(0), None)
+    
+    def test_rentBikeOnHourlyBasis_for_valid_positive_number_of_bikes(self):
+        shop = BikeRental(10)
+        now = datetime.now().hour
+        self.assertEqual(shop.rentBikeOnHourlyBasis(2).hour, now)
+    
+    def test_rentBikeOnHourlyBasis_for_invalid_positive_number_of_bikes(self):
+        shop = BikeRental(10)
+        self.assertEqual(shop.rentBikeOnHourlyBasis(11), None)
+    
+    def test_rentBikeOnDailyBasis_for_negative_number_of_bikes(self):
+        shop = BikeRental(10)
+        self.assertEqual(shop.rentBikeOnDailyBasis(-1), None)
+    
+    def test_rentBikeOnDailyBasis_for_zero_number_of_bikes(self):
+        shop = BikeRental(10)
+        self.assertEqual(shop.rentBikeOnDailyBasis(0), None)
+    
+    def test_rentBikeOnDailyBasis_for_valid_positive_number_of_bikes(self):
+        shop = BikeRental(10)
+        hour = datetime.now().hour
+        self.assertEqual(shop.rentBikeOnDailyBasis(2).hour, hour)
+    
+    def test_rentBikeOnDailyBasis_for_invalid_positive_number_of_bikes(self):
+        shop = BikeRental(10)
+        self.assertEqual(shop.rentBikeOnDailyBasis(11), None)
+    
+    def test_rentBikeOnWeeklyBasis_for_negative_number_of_bikes(self):
+        shop = BikeRental(10)
+        self.assertEqual(shop.rentBikeOnDailyBasis(-1), None)
+    
+    def test_rentBikeOnWeeklyBasis_for_zero_number_of_bikes(self):
+        shop = BikeRental(10)
+        self.assertEqual(shop.rentBikeOnDailyBasis(0), None)
+    
+    def test_rentBikeOnWeeklyBasis_for_valid_positive_number_of_bikes(self):
+        shop = BikeRental(10)
+        now = datetime.now().hour
+        self.assertEqual(shop.rentBikeOnDailyBasis(2).hour, now)
+    
+    def test_rentBikeOnWeeklyBasis_for_invalid_positive_number_of_bikes(self):
+        shop = BikeRental(10)
+        self.assertEqual(shop.rentBikeOnDailyBasis(11), None)
+    
+    def test_renturnBike_for_invalid_rentalTime(self):
+        # Create a shop and a customer
+        shop = BikeRental(10)
+        customer = Customer()
+        
+        # Let customer not rent a bike and try to return a bike
+        request = customer.returnBike()
+        self.
+        
